8d0fa57977b220458de50f4b64cbce4cfef4b897
rafuj <raihanrafuj9978@gmail.com>
2022-02-11 00:51:28 +0600

    Functionality  Completed, Sidebar and Modal Toggle Successfully

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { FaTimes } from "react-icons/fa";
+import { useGlobalContext } from "./context";
 import { links, social } from "./data";
 import logo from "./logo.svg";
 const Sidebar = () => {
+	const { isSidebarOpen, closeSidebar } = useGlobalContext();
+
 	return (
-		<aside className={`sidebar`}>
+		<aside className={`sidebar ${isSidebarOpen && "show-sidebar"}`}>
 			<div className="sidebar-header">
 				<img src={logo} alt="logo" />
-				<button className="close-btn">
+				<button className="close-btn" onClick={closeSidebar}>
 					<FaTimes />
 				</button>
 			</div>
